4b80364f331279c6a71eda218df0e19d396ea6ea
Nock27 <nock5631@gmail.com>
2023-07-17 11:27:38 +0300

    17.07.2023

--- Advanced Functions - Exercise/08. Next Article/script.js
+++ Advanced Functions - Exercise/08. Next Article/script.js
@@ -1,3 +1,21 @@
-function getArticleGenerator(articles) {
-    // TODO
+function getArticleGenerator(input) {
+
+    let articles = input;
+    
+    return () => { 
+        if(articles){
+            let parentDiv = document.getElementById('content');
+            let article = document.createElement('article');
+            article.innerText = articles.shift();
+            parentDiv.appendChild(article);
+        }
 }
+}
+
+// while(articles.length){
+//     let parentDiv = document.getElementById('content');
+//     let article = document.createElement('article');
+//     article.innerText = articles.shift();
+//     parentDiv.appendChild(article);
+//     break;
+// }
