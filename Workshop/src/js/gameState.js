93349994f7aabb77559b3709a017206d50992c61
Божидар Вълков <125079323+Nock27@users.noreply.github.com>
2023-08-08 17:43:54 +0300

    08.08.2023

--- Workshop/src/js/gameState.js
+++ Workshop/src/js/gameState.js
@@ -1,6 +1,42 @@
 function initState() {
+    let startX = Math.floor(Math.random() * 1000);
+    let startY = Math.floor(Math.random() * 500);
+    
     const state = {
-        player : 'Sarasfati',
+        player: 'Sarasfati',
+        gameOver: false,
+        score: 0,
+        scoreRate: 1,
+        killScore: 1000,
+        wizard: {
+            width: 170,
+            height: 100,
+            posX: startX,
+            posY: startY,
+            speed: 10,
+        },
+        bugStats: {
+            width: 75,
+            height: 75,
+            nextSpawnTimestamp: 0,
+            maxSpawnInterval: 1500,
+            speed: 8,
+        },
+        fireball: {
+            width: 70,
+            height: 40,
+            speed: 12,
+            nextSpawnTimestamp: 0,
+            fireRate: 500,
+        },
+        keys: {
+            KeyA: false,
+            KeyS: false,
+            KeyD: false,
+            KeyW: false,
+            Space: false,
+        }
     }
+
     return state;
 }
