4f15a351166f469df4dcf32274f211068523b444
Божидар Вълков <125079323+Nock27@users.noreply.github.com>
2023-08-26 14:57:49 +0300

    Added the functionality to visualize the movies in the home page

--- ^JS APPLICATION^/Single Page Applications - Exercise/Exercise/Resource-25862-01.SoftForum/02.Movies/src/util.js
+++ ^JS APPLICATION^/Single Page Applications - Exercise/Exercise/Resource-25862-01.SoftForum/02.Movies/src/util.js
@@ -1,9 +1,17 @@
 
 const views = [...document.querySelectorAll('.view-section')];
+
 function hideAll(){
     views.forEach(x => x.style.display = 'none');
 }
 export function showView(section){
     hideAll();
     section.style.display = 'block';
+}
+
+export function spinner(){
+    const element = document.createElement('p');
+    element.innerHTML = 'Loading &hellip;'
+
+    return element;
 }
