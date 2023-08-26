c555246d45088f92720d3283c6085f9a9ff33d99
Божидар Вълков <125079323+Nock27@users.noreply.github.com>
2023-08-26 14:17:34 +0300

    Added app.js file that works like a router,made few js files for every page in the web app and connected them with the router

---
+++ ^JS APPLICATION^/Single Page Applications - Exercise/Exercise/Resource-25862-01.SoftForum/02.Movies/src/app.js
@@ -0,0 +1,59 @@
+// [x] improve HTML structure
+// [x] create app.js module
+// [x] create util.js containing hide and display of view
+// [x] placeholders for all views
+
+
+// implement views
+// - create request logic
+// - DOM manipulation logic
+
+// [] catalog
+// [] login
+// [] register
+// [] create
+// [] details
+// [] like
+// [] edit
+// [] delete
+
+import { homePage } from "./home.js";
+import { loginPage } from "./login.js";
+import { registerPage } from "./register.js";
+import { createPage } from "./create.js";
+
+
+const routes = {
+  '/': homePage,
+  '/login' : loginPage,
+  '/logout' : logout,
+  '/register' : registerPage,
+  '/create' : createPage,
+
+};
+
+document.querySelector('nav').addEventListener('click',onNavigate);
+document.querySelector('#add-movie-button a').addEventListener('click', onNavigate);
+function onNavigate(e){
+    if(e.target.tagName == 'A' && e.target.href){
+        e.preventDefault();
+        const url = new URL(e.target.href);
+
+        const view = routes[url.pathname]
+        if(typeof view == 'function'){
+            view();
+        }
+        console.log(url.pathname);
+
+    }
+}
+
+
+
+
+function logout(){
+    alert('Logged out')
+}
+
+//Start application in catalog view\
+homePage();
---
+++ ^JS APPLICATION^/Single Page Applications - Exercise/Exercise/Resource-25862-01.SoftForum/02.Movies/src/util.js
@@ -0,0 +1,9 @@
+
+const views = [...document.querySelectorAll('.view-section')];
+function hideAll(){
+    views.forEach(x => x.style.display = 'none');
+}
+export function showView(section){
+    hideAll();
+    section.style.display = 'block';
+}
